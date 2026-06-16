8bff8377743ba6c45ad47cbd4e188ca324864c70
Adeel App <adeelchatcp2026@gmail.com>
2026-06-16 15:19:43 +0000

    Switch deployment target to Firebase Hosting only. Remove App Hosting c

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zynqo-cache-v1';
+const CACHE_NAME = 'zynqo-v1';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
@@ -11,6 +11,16 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
     })
   );
 });
